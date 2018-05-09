4700768752fca66fe02ad306894cef46c260334f
yuhuashang <yuhuashang@aliyun.cfom>
2018-05-09 16:49:58 +0800

    Initial commit

---
+++ src/main/java/com/yuhuashang/eos/EosApplication.java
@@ -0,0 +1,17 @@
+package com.yuhuashang.eos;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EosApplication {
+
+	public static void main(String[] args) {
+
+		new SpringApplicationBuilder(EosApplication.class)
+				.web(true).run(args);
+	}
+}
